2b350ef8dcd91307ec292a924f87a15dc45deef1
Jacob Starko <105564503+Fatalerorr69@users.noreply.github.com>
2026-02-20 05:12:20 +0100

    Create fail2ban.sh

---
+++ security/fail2ban.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat > /etc/fail2ban/jail.local <<EOF
+[sshd]
+enabled = true
+port = ssh
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 3
+bantime = 1h
+EOF
+
+systemctl enable --now fail2ban
